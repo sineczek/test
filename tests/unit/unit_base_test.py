c5af308bffd3cf99bacf0923bfe0e352a76b0eb1
Michal Zaitz <michalzaitz@gmail.com>
2021-04-19 20:21:34 +0200

    Dzień #111 travis test3

--- tests/unit/unit_base_test.py
+++ tests/unit/unit_base_test.py
@@ -4,6 +4,7 @@ BaseTest
 This class should be the parent class to each unit test.
 It allows for instantiation of the database dynamically,
 and makes sure that it is a new, blank database each time.
+jakieś drobne zmiany
 """
 
 from unittest import TestCase
